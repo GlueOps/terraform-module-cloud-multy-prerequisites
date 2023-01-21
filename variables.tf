69407e1f311409f7e8dcdec02f0510a955e0b634
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-01-21 01:30:11 +0000

    feat: working module that uses cloudflare provider at the moment.

---
+++ variables.tf
@@ -0,0 +1,32 @@
+
+variable "domain_to_delegate_from" {
+  description = "The domain name of the domain that all delegation is coming from"
+  type        = string
+  nullable    = false
+}
+
+variable "company_key" {
+  description = "The company key"
+  type        = string
+  nullable    = false
+}
+
+variable "company_account_id" {
+  description = "The company AWS account id"
+  type        = string
+  nullable    = false
+}
+
+variable "cluster_environments" {
+  description = "The cluster environments"
+  type        = list(string)
+  nullable    = false
+}
+
+locals {
+  domain_to_delegate_from = var.domain_to_delegate_from
+  company_key             = var.company_key
+  ns_record_ttl           = "60"
+  ns_record_type          = "NS"
+  aws_region              = "us-west-2"
+}
