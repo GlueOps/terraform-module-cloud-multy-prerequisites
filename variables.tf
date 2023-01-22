c1eae24d873063cf9710308392d01aaef83e6cff
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-01-22 10:01:23 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,6 +5,13 @@ variable "domain_to_delegate_from" {
   nullable    = false
 }
 
+variable "allow_force_destroy_of_cluster_zones" {
+  description = "Allow cluster related zones that have records in them to be destroyed"
+  type        = string
+  nullable    = false
+  default     = false
+}
+
 variable "company_key" {
   description = "The company key"
   type        = string
