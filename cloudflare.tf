37bdcffa26f5481d6fcf39d0bd0c31a488bfaa3a
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-02-08 19:42:33 +0000

    fixes

--- cloudflare.tf
+++ cloudflare.tf
@@ -8,7 +8,7 @@ resource "cloudflare_record" "delegation_ns_record_first" {
   name     = aws_route53_zone.main.name
   value    = aws_route53_zone.main.name_servers[0]
   type     = local.ns_record_type
-  ttl      = local.ns_record_ttl
+  ttl      = local.record_ttl
   proxied  = false
   priority = null
 }
