5bca50fa062e6ece410a44cfa820d4c648c597f2
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-03-04 01:18:55 +0000

    feat: module provider configuration

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -1,5 +1,9 @@
 module "common_s3" {
   source = "./modules/multy-s3-bucket/0.1.0"
+  providers = {
+    aws.primaryregion = aws.primaryregion
+    aws.replicaregion = aws.replicaregion
+  }
 
   bucket_name         = local.bucket_name
   this_is_development = var.this_is_development
@@ -9,7 +13,11 @@ module "common_s3" {
 }
 
 module "loki_s3" {
-  source   = "./modules/multy-s3-bucket/0.1.0"
+  source = "./modules/multy-s3-bucket/0.1.0"
+  providers = {
+    aws.primaryregion = aws.primaryregion
+    aws.replicaregion = aws.replicaregion
+  }
   for_each = aws_route53_zone.clusters
 
   bucket_name         = "${local.bucket_name}-${aws_route53_zone.clusters[each.key].name}-loki"
