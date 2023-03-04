3d5fbea13201fe0627f264518df5e8665feb51ed
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-03-04 01:26:09 +0000

    chore: clean up

--- modules/multy-s3-bucket/0.1.0/providers.tf
+++ modules/multy-s3-bucket/0.1.0/providers.tf
@@ -11,21 +11,3 @@ terraform {
     }
   }
 }
-
-
-# provider "aws" {
-#   alias  = "primaryregion"
-#   region = var.primary_region
-#   assume_role {
-#     role_arn = "arn:aws:iam::${var.company_account_id}:role/OrganizationAccountAccessRole"
-#   }
-# }
-
-# provider "aws" {
-#   alias  = "replicaregion"
-#   region = var.backup_region
-#   assume_role {
-#     role_arn = "arn:aws:iam::${var.company_account_id}:role/OrganizationAccountAccessRole"
-#   }
-# }
-
--- modules/multy-s3-bucket/0.1.0/variables.tf
+++ modules/multy-s3-bucket/0.1.0/variables.tf
@@ -29,6 +29,3 @@ variable "backup_region" {
   type        = string
   nullable    = false
 }
-
-
-
