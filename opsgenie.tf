e04ebc72926e888856024d12dda8693bfbcada05
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-21 16:26:48 -0700

    Update opsgenie.tf

--- opsgenie.tf
+++ opsgenie.tf
@@ -4,3 +4,10 @@ module "opsgenie_teams" {
   company_key          = var.company_key
   cluster_environments = var.cluster_environments
 }
+
+output "opsgenie_prometheus_api_keys" {
+  value = {
+    for env_key, env_value in local.cluster_environments_set : env_key => opsgenie_teams.opsgenie_prometheus_api_keys[env_key]
+  }
+  description = "A map of the Opsgenie API keys for the Prometheus integrations."
+}
