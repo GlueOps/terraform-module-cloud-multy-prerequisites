76ab137fba0e24a0317d0106011d4a53359682c4
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-28 06:42:12 +0000

    enabling dns sec

--- main.tf
+++ main.tf
@@ -1,7 +1,46 @@
+data "aws_route53_zone" "management_tenant_dns" {
+  provider = aws.management-tenant-dns
+  zone_id  = local.management_tenant_dns_zoneid
+}
+
 resource "aws_route53_zone" "main" {
   provider = aws.clientaccount
   name     = "${local.company_key}.${data.aws_route53_zone.management_tenant_dns.name}"
 }
+
+resource "aws_route53_record" "delegation_to_parent_tenant_zone" {
+  provider = aws.management-tenant-dns
+  zone_id  = data.aws_route53_zone.management_tenant_dns.zone_id
+  name     = data.aws_route53_zone.management_tenant_dns.name
+  type     = local.ns_record_type
+  ttl      = local.record_ttl
+  records  = aws_route53_zone.main.name_servers
+}
+
+
+
+module "dnssec_key" {
+  source         = "git::https://github.com/GlueOps/terraform-module-cloud-aws-dnssec-kms-key.git?ref=feat/adding-kms"
+  aws_account_id = var.company_account_id
+}
+
+resource "aws_route53_key_signing_key" "parent_tenant_zone" {
+  provider = aws.clientaccount
+  hosted_zone_id             = aws_route53_zone.main.zone_id
+  key_management_service_arn = module.dnssec_key.kms_key_arn
+  name                       = "primary"
+  status                     = "ACTIVE"
+}
+
+resource "aws_route53_record" "enable_dnssec_parent_tenant_zone" {
+  provider = aws.management-tenant-dns
+  zone_id  = data.aws_route53_zone.management_tenant_dns
+  name     = data.aws_route53_zone.management_tenant_dns.name
+  type     = "DS"
+  ttl      = local.record_ttl
+  records  = aws_route53_key_signing_key.parent_tenant_zone.ds_record
+}
+
 
 resource "aws_route53_record" "wildcard_for_apps" {
   provider = aws.clientaccount
