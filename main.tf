117b2a89eae41793da600f189eb3eb220655123d
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-28 06:52:41 +0000

    fixes

--- main.tf
+++ main.tf
@@ -25,20 +25,60 @@ module "dnssec_key" {
 }
 
 resource "aws_route53_key_signing_key" "parent_tenant_zone" {
-  provider = aws.clientaccount
+  provider                   = aws.clientaccount
   hosted_zone_id             = aws_route53_zone.main.zone_id
   key_management_service_arn = module.dnssec_key.kms_key_arn
   name                       = "primary"
   status                     = "ACTIVE"
 }
 
-resource "aws_route53_record" "enable_dnssec_parent_tenant_zone" {
+resource "aws_route53_record" "enable_dnssec_for_parent_tenant_zone" {
   provider = aws.management-tenant-dns
   zone_id  = data.aws_route53_zone.management_tenant_dns.zone_id
   name     = data.aws_route53_zone.management_tenant_dns.name
   type     = "DS"
   ttl      = local.record_ttl
   records  = [aws_route53_key_signing_key.parent_tenant_zone.ds_record]
+}
+
+
+resource "aws_route53_zone" "clusters" {
+  provider = aws.clientaccount
+  for_each = toset(var.cluster_environments)
+  name     = "${each.value}.${local.company_key}.${data.aws_route53_zone.management_tenant_dns.name}"
+  depends_on = [
+    aws_route53_zone.main
+  ]
+  force_destroy = var.this_is_development ? true : false
+}
+
+resource "aws_route53_key_signing_key" "cluster_zones" {
+  provider                   = aws.clientaccount
+  for_each                   = aws_route53_zone.clusters
+  hosted_zone_id             = aws_route53_zone.clusters[each.key].zone_id
+  key_management_service_arn = module.dnssec_key.kms_key_arn
+  name                       = "primary"
+  status                     = "ACTIVE"
+}
+
+resource "aws_route53_record" "cluster_zone_dnssec_records" {
+  provider = aws.clientaccount
+  for_each = aws_route53_zone.clusters
+  zone_id  = aws_route53_zone.main.zone_id
+  name     = each.value.name
+  type     = "DS"
+  ttl      = local.record_ttl
+  records  = aws_route53_key_signing_key.cluster_zones[each.key].ds_record
+}
+
+resource "aws_route53_record" "cluster_zone_ns_records" {
+  provider = aws.clientaccount
+  for_each = aws_route53_zone.clusters
+  zone_id  = aws_route53_zone.main.zone_id
+  name     = each.value.name
+  type     = local.ns_record_type
+  ttl      = local.record_ttl
+  records  = aws_route53_zone.clusters[each.key].name_servers
 }
 
 
@@ -51,25 +91,4 @@ resource "aws_route53_record" "wildcard_for_apps" {
   ttl      = local.record_ttl
   records  = ["ingress.${each.value.name}"]
 }
-
-resource "aws_route53_zone" "clusters" {
-  provider = aws.clientaccount
-  for_each = toset(var.cluster_environments)
-  name     = "${each.value}.${local.company_key}.${data.aws_route53_zone.management_tenant_dns.name}"
-  depends_on = [
-    aws_route53_zone.main
-  ]
-  force_destroy = var.this_is_development ? true : false
-}
-
-resource "aws_route53_record" "cluster_subdomain_ns_records" {
-  provider = aws.clientaccount
-  for_each = aws_route53_zone.clusters
-  zone_id  = aws_route53_zone.main.zone_id
-  name     = each.value.name
-  type     = local.ns_record_type
-  ttl      = local.record_ttl
-  records  = aws_route53_zone.clusters[each.key].name_servers
-}
-
 
