a1f2d30f24dcb13bcae4d4d053f326e40acbe14f
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-28 06:12:31 +0000

    testing

--- save-credentials-to-s3.tf
+++ save-credentials-to-s3.tf
@@ -1,17 +1,17 @@
-# resource "aws_s3_bucket_object" "combined_outputs" {
-#   for_each     = {
-#     for name in toset([for k in keys({ for user, keys in aws_iam_access_key.certmanager : aws_route53_zone.clusters[user].name => keys }) : k]) :
-#     name => {
-#       certmanager_credentials = { for user, keys in aws_iam_access_key.certmanager : aws_route53_zone.clusters[user].name => keys }
-#       externaldns_credentials = { for user, keys in aws_iam_access_key.externaldns : aws_route53_zone.clusters[user].name => keys }
-#       loki_credentials        = { for user, keys in aws_iam_access_key.loki_s3 : aws_route53_zone.clusters[user].name => keys }
-#       opsgenie_credentials    = lookup(module.opsgenie_teams.opsgenie_prometheus_api_keys, split(".", name)[0], null)
-#       vault_credentials       = { for user, keys in aws_iam_access_key.vault_s3 : aws_route53_zone.clusters[user].name => keys }
-#     }
-#   }
-#   provider     = aws.primaryregion
-#   bucket       = module.common_s3.primary_s3_bucket_id
-#   key          = "${each.key}/configurations/credentials.json"
-#   content      = jsonencode(each.value)
-#   content_type = "application/json"
-# }
+resource "aws_s3_bucket_object" "combined_outputs" {
+  for_each     = {
+    for ame in var.cluster_environments :
+    name => {
+      certmanager_credentials = { for user, keys in aws_iam_access_key.certmanager : aws_route53_zone.clusters[user].name => keys }
+      externaldns_credentials = { for user, keys in aws_iam_access_key.externaldns : aws_route53_zone.clusters[user].name => keys }
+      loki_credentials        = { for user, keys in aws_iam_access_key.loki_s3 : aws_route53_zone.clusters[user].name => keys }
+      opsgenie_credentials    = lookup(module.opsgenie_teams.opsgenie_prometheus_api_keys, split(".", name)[0], null)
+      vault_credentials       = { for user, keys in aws_iam_access_key.vault_s3 : aws_route53_zone.clusters[user].name => keys }
+    }
+  }
+  provider     = aws.primaryregion
+  bucket       = module.common_s3.primary_s3_bucket_id
+  key          = "${each.key}/configurations/credentials.json"
+  content      = jsonencode(each.value)
+  content_type = "application/json"
+}
