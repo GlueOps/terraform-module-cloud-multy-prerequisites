acdf051c88e66b9c675f1b8280f7f9af59d9a6d3
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-04-27 06:47:19 +0000

    fixes

--- iam-policy-loki-xptr-s3.tf
+++ iam-policy-loki-xptr-s3.tf
@@ -10,10 +10,9 @@ resource "aws_iam_policy" "loki_logs_exporter_s3" {
       "Effect": "Allow",
       "Action": [
         "s3:PutObject",
-        "s3:ListObject",
+        "s3:List*",
         "s3:HeadObject",
-        "s3:GetObject",
-        "s3:GetObjectAttributes"
+        "s3:Get*",
       ],
       "Resource": [
         "${module.common_s3.primary_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/loki_exported_logs/*",
