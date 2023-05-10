9ba64a12dd471933d23ec7b48dc14f3544c8cf85
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-10 06:18:56 +0000

    fixes

--- captain-repo.tf
+++ captain-repo.tf
@@ -3,8 +3,10 @@ module "captain_repository" {
   source         = "./modules/github-captain-repository"
   repostory_name = "${each.value}.${aws_route53_zone.main.name}"
   files_to_create = {
-    "argocd.yaml"   = module.argocd_helm_values[each.value].helm_values
-    "platform.yaml" = module.glueops_platform_helm_values[each.value].helm_values
-
+    "argocd.yaml"                          = module.argocd_helm_values[each.value].helm_values
+    "platform.yaml"                        = module.glueops_platform_helm_values[each.value].helm_values
+    "terraform/kubernetes/main.tf"         = "//tf goes here"
+    "terraform/vault/vault-init/main.tf"   = "//tf goes here"
+    "terraform/vault/vault-config/main.tf" = "//tf goes here"
   }
 }
