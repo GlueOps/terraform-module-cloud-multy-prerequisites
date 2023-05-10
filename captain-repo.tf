71021d72cbe9bfe5220d257289afd73dff27d53b
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-10 06:12:37 +0000

    fixes

--- captain-repo.tf
+++ captain-repo.tf
@@ -4,7 +4,7 @@ module "captain_repository" {
   repostory_name = "${each.value}.${aws_route53_zone.main.name}"
   files_to_create = {
     "argocd.yaml"   = module.argocd_helm_values[each.value].helm_values
-    "platform.yaml" = module.platform_helm_values[each.value].helm_values
+    "platform.yaml" = module.glueops_platform_helm_values[each.value].helm_values
 
   }
 }
