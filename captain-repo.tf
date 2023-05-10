8bb8e5fbcaa21d8efa8017b50ca20c2f6b20d4b4
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-10 06:07:13 +0000

    fixes

--- captain-repo.tf
+++ captain-repo.tf
@@ -2,8 +2,7 @@ module "captain_repository" {
   for_each       = toset(["captain-repo-test1"])
   source         = "./modules/github-captain-repository"
   repostory_name = each.key
-  files_to_create = [
-    {
+  files_to_create = {
       "README.md" = "This is a README file"
-  }]
+  }
 }
