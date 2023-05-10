734aece5878f9b11473a67fb40b7c411f8916ca2
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-10 17:54:45 +0000

    fixes

--- captain-repo.tf
+++ captain-repo.tf
@@ -10,16 +10,18 @@ module "captain_repository" {
 module "initialize_vault_cluster" {
   source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git"
 }
+
 EOT
     "terraform/vault/vault-config/main.tf" = <<EOT
 module "configure_vault_cluster" {
     source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git"
     oidc_client_secret = "${random_password.dex_vault_client_secret[each.key].result}"
     captain_domain = "${each.value}.${aws_route53_zone.main.name}"
-    #org_team_policy_mappings = replace(${jsonencode(var.vault_github_org_team_policy_mappings)}, ""oidc_groups" 
-  org_team_policy_mappings = [
-    ${join(",\n    ", [for mapping in var.vault_github_org_team_policy_mappings : "{ oidc_groups = ${jsonencode(mapping.oidc_groups)}, policy_name = \"${mapping.policy_name}\" }"])}
+    org_team_policy_mappings = [
+      ${join(",\n    ", [for mapping in var.vault_github_org_team_policy_mappings : "{ oidc_groups = ${jsonencode(mapping.oidc_groups)}, policy_name = \"${mapping.policy_name}\" }"])}
+    ]
 }
+
 EOT
   }
 }
