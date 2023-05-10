3ff3f03ad550c187e1fdabf995593247d0a6064a
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-10 05:48:01 +0000

    testing

--- captain-repo.tf
+++ captain-repo.tf
@@ -1,4 +1,4 @@
-module "captain_repository" {
+module "captain_repository_test" {
   source         = "./modules/github-captain-repository"
   repostory_name = "captain-repo-test1"
   organization   = "development-captains"
