4e4ec4ddee901379a0583649c14398d750f8cce5
Venkata Mutyala <venkata@venkatamutyala.com>
2023-06-13 18:38:05 -0700

    updates to not have pinned versions

--- captain-repo.tf
+++ captain-repo.tf
@@ -1,3 +1,13 @@
+locals {
+
+  provider_versions_tf_file = << EOT
+module "provider_versions" {
+  source = "git::https://github.com/GlueOps/terraform-module-provider-versions.git"
+}
+EOT
+
+}
+
 module "captain_repository" {
   for_each        = local.environment_map
   source          = "./modules/github-captain-repository/0.1.0"
@@ -6,7 +16,10 @@ module "captain_repository" {
     "argocd.yaml"                            = module.argocd_helm_values[each.value.environment_name].helm_values
     "platform.yaml"                          = module.glueops_platform_helm_values[each.value.environment_name].helm_values
     "README.md"                              = module.tenant_readmes[each.value.environment_name].tenant_readme
-    "terraform/kubernetes/.gitkeep"          = ""
+    "terraform/kubernetes/provider_versions.tf"          = local.provider_versions_tf_file
+    "terraform/vault/initialization/provider_versions.tf"          = local.provider_versions_tf_file
+    "terraform/vault/configuration/provider_versions.tf"          = local.provider_versions_tf_file
+
     ".gitignore"                             = <<EOT
 
 .terraform
@@ -15,13 +28,13 @@ module "captain_repository" {
 EOT
     "terraform/vault/initialization/main.tf" = <<EOT
 module "initialize_vault_cluster" {
-  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.3.0"
+  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.4.0"
 }
 
 EOT
     "terraform/vault/configuration/main.tf"  = <<EOT
 module "configure_vault_cluster" {
-    source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.4.3"
+    source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.5.1"
     oidc_client_secret = "${random_password.dex_vault_client_secret[each.key].result}"
     captain_domain = "${each.value.environment_name}.${aws_route53_zone.main.name}"
     org_team_policy_mappings = [
