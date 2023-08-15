ed6d1950a6a00786c0c567aaed798e9e6948223f
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-15 01:33:25 +0000

    fixes

--- iam-policy-vault-init-s3.tf
+++ iam-policy-vault-init-s3.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "vault_init_s3" {
   provider = aws.clientaccount
   for_each = aws_route53_zone.clusters
-  name     = "vault-s3-${aws_route53_zone.clusters[each.key].name}"
+  name     = "vault-init-s3-${aws_route53_zone.clusters[each.key].name}"
   policy   = <<EOF
 {
   "Version": "2012-10-17",
