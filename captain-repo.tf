f255a8cf329ea827128cfc04e62c7b6e4f028640
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-17 23:48:23 +0000

    wip

--- captain-repo.tf
+++ captain-repo.tf
@@ -18,6 +18,11 @@ module "captain_repository" {
 
 }
 
+local {
+
+  vault_init_s3_key = "${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-init/vault_access.json"
+}
+
 module "captain_repository_files" {
   for_each        = local.environment_map
   source          = "./modules/github-captain-repository-files/0.1.0"
@@ -53,7 +58,7 @@ module "configure_vault_cluster" {
 
     aws_region     = "${var.primary_region}"
     aws_s3_bucket_name  = "${module.common_s3.primary_s3_bucket_id}"
-    aws_s3_key_vault_secret_file     = "${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-init/vault_access.json"
+    aws_s3_key_vault_secret_file     = ${local.vault_init_s3_key}
     aws_access_key = "${aws_iam_access_key.vault_init_s3[each.value.environment_name].id}"
     aws_secret_key =   "${aws_iam_access_key.vault_init_s3[each.value.environment_name].secret}"
 }
