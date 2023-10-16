05cf4bb401d53a4610a6cf7894cb2181a0bffd9a
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-10-16 12:13:00 +0300

    update to v0.34.0-rc7

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -42,7 +42,7 @@ locals {
   codespace_version         = "v0.31.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.45.4"
-  glueops_platform_version  = "v0.34.0-rc6" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  glueops_platform_version  = "v0.34.0-rc7" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
   tools_version             = "v0.5.0"
 }
 
