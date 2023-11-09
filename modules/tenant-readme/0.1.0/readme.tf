e47145ca1a9e862d3bdc834bf4fbb1dcb444fba7
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-11-08 21:40:15 -0700

    chore(codespaces): v0.31.3 -> v0.31.4

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -39,7 +39,7 @@ data "local_file" "readme" {
 }
 
 locals {
-  codespace_version         = "v0.31.3"
+  codespace_version         = "v0.31.4"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.50.1"
   glueops_platform_version  = "v0.35.0-rc9" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
