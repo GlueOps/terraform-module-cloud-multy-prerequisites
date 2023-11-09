90ea07860027c85c39555f4d0fffd6da0da77f15
Venkata Mutyala <venkata@venkatamutyala.com>
2023-11-09 18:31:19 +0000

    fixes

--- modules/opsgenie/0.1.0/heartbeat.tf
+++ modules/opsgenie/0.1.0/heartbeat.tf
@@ -1,10 +1,10 @@
 resource "opsgenie_heartbeat" "captain" {
   for_each      = local.cluster_environments_set
-  name          = "${each.value}.${var.tenant_key}.${var.domain}"
-  description   = "${each.value}.${var.tenant_key}.${var.domain}"
+  name          = "${each.value}.${var.domain}"
+  description   = "${each.value}.${var.domain}"
   interval_unit = "minutes"
   interval      = 2
   enabled       = true
-  alert_message = "Heartbeat Expired: ${each.value}.${var.tenant_key}.${var.domain}"
+  alert_message = "Heartbeat Expired: ${each.value}.${var.domain}"
   owner_team_id = opsgenie_team.teams[each.key].id
 }
