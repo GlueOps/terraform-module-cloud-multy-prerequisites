d46be929f4dd3e06647be61ef16cd50bc0dd8cd6
Venkat <venkata@venkatamutyala.com>
2025-05-02 11:12:34 +0000

    fix: multi region access point

--- modules/multy-s3-bucket/0.2.0/s3-multi-region-access-point.tf
+++ modules/multy-s3-bucket/0.2.0/s3-multi-region-access-point.tf
@@ -9,12 +9,13 @@ resource "aws_s3control_multi_region_access_point" "s3_multi_region_access_point
     region {
       bucket = aws_s3_bucket.replica.id
     }
-  }
-  public_access_block = {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+
+    public_access_block = {
+      block_public_acls       = true
+      block_public_policy     = true
+      ignore_public_acls      = true
+      restrict_public_buckets = true
+    }
   }
 }
 
