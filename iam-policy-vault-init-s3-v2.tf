c828958d33c8c621e918457246e58dc895c7a3e5
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-05-02 08:17:54 +0000

    update vaul-init

---
+++ iam-policy-vault-init-s3-v2.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_policy" "vault_init_s3_v2" {
+  provider = aws.clientaccount
+  for_each = aws_route53_zone.clusters
+  name     = random_uuid.vault_init_s3_v2_aws_iam_policy[each.key].result
+  policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject*",
+        "s3:List*"
+      ],
+      "Resource": [
+        "${module.common_s3.primary_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-init/*",
+        "${module.common_s3.replica_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-init/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject*",
+        "s3:List*"
+      ],
+      "Resource": [
+        "${module.common_s3.primary_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-backups/*",
+        "${module.common_s3.replica_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/hashicorp-vault-backups/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "${module.common_s3.primary_s3_bucket_arn}",
+        "${module.common_s3.replica_s3_bucket_arn}"
+      ]
+    }
+  ]
+}
+EOF
+  tags = {
+    Name = "vault-init-s3-${aws_route53_zone.clusters[each.key].name}"
+  }
+}
+
+
+resource "random_uuid" "vault_init_s3_v2_aws_iam_policy" {
+  for_each = aws_route53_zone.clusters
+}
