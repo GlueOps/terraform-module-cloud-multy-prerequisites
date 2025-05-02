9d8f80448864efbb84806d18705c32267f2b176e
Venkat <venkata@venkatamutyala.com>
2025-05-02 07:39:28 +0000

    feat: adding tls cert backup using random_uuid

---
+++ iam-policy-tls-cert-backup-s3-v2.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_policy" "tls_cert_backup_s3_v2" {
+  provider = aws.clientaccount
+  for_each = aws_route53_zone.clusters
+  name     = random_uuid.tls_cert_backup_s3_v2_aws_iam_policy[each.key].result
+  policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:List*"
+      ],
+      "Resource": [
+        "${module.common_s3.primary_s3_bucket_arn}/${aws_route53_zone.clusters[each.key].name}/tls-cert-backups/*"
+      ]
+    }
+  ]
+}
+EOF
+  tags = {
+    Name = "tls-cert-backup-s3-${aws_route53_zone.clusters[each.key].name}"
+  }
+}
+
+
+resource "random_uuid" "tls_cert_backup_s3_v2_aws_iam_policy" {
+  for_each = aws_route53_zone.clusters
+}
