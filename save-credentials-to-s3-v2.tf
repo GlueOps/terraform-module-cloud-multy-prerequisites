59237e319f2d4e52d4334b2d613eb3931594f63b
Venkat <venkata@venkatamutyala.com>
2025-05-03 03:37:03 -0700

    fix: remove unused credentials from aws_s3_object.combined_outputs_v2

--- save-credentials-to-s3-v2.tf
+++ save-credentials-to-s3-v2.tf
@@ -6,10 +6,6 @@ resource "aws_s3_object" "combined_outputs_v2" {
   bucket   = module.common_s3_v2.s3_multi_region_access_point_arn
   key      = "${each.value}.${aws_route53_zone.main.name}/configurations/credentials.json"
   content = jsonencode({
-    certmanager_credentials = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.certmanager_v2[each.value] },
-    externaldns_credentials = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.externaldns_v2[each.value] },
-    loki_credentials        = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.loki_s3_v2[each.value] },
-    opsgenie_credentials    = lookup(module.opsgenie_teams.opsgenie_prometheus_api_keys, split(".", each.value)[0], null),
     vault_credentials       = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.vault_s3_backup_v2[each.value] },
   })
 
