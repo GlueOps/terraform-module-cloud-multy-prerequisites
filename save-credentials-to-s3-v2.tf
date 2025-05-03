78250160a2a957f045edabac0a7b7943b304dacd
Venkat <venkata@venkatamutyala.com>
2025-05-03 22:11:02 +0000

    chore: run tf fmt

--- save-credentials-to-s3-v2.tf
+++ save-credentials-to-s3-v2.tf
@@ -6,7 +6,7 @@ resource "aws_s3_object" "combined_outputs_v2" {
   bucket   = module.common_s3_v2.s3_multi_region_access_point_arn
   key      = "${each.value}.${aws_route53_zone.main.name}/configurations/credentials.json"
   content = jsonencode({
-    vault_credentials       = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.vault_s3_backup_v2[each.value] },
+    vault_credentials = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.vault_s3_backup_v2[each.value] },
   })
 
   content_type           = "application/json"
