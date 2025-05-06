e8c07d6cf73695476c07f07300557cd7f1bd78ed
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-05-06 15:52:33 +0000

    changed  lifecycle

--- generate-helm-values.tf
+++ generate-helm-values.tf
@@ -42,8 +42,8 @@ resource "random_password" "grafana_admin_secret" {
 }
 
 locals {
-  vault_access_tokens_s3_key          = "hashicorp-vault-init/vault_access.json"
-  tls_cert_backup_s3_key_prefix       = "tls-cert-backups"
+  vault_access_tokens_s3_key          = "backups_with_expiration_enabled/hashicorp-vault-init/vault_access.json"
+  tls_cert_backup_s3_key_prefix       = "backups_with_expiration_enabled/tls-cert-backups"
   tls_cert_restore_exclude_namespaces = "kube-system"
 }
 
--- modules/multy-s3-bucket/0.1.0/s3-backup-bucket.tf
+++ modules/multy-s3-bucket/0.1.0/s3-backup-bucket.tf
@@ -31,12 +31,56 @@ resource "aws_s3_bucket_lifecycle_configuration" "replica" {
   depends_on = [aws_s3_bucket_versioning.replica]
   bucket     = aws_s3_bucket.replica.id
   rule {
-    id = "expire_non_current_version"
+    id = "expire_transition_vault"
 
-    filter {}
+    filter {
+      prefix = "backups_with_expiration_enabled/hashicorp-vault-backups/"
+    }
+
+    expiration {
+      days = 180
+    }
+
+    transition {
+      days          = var.this_is_development ? 30 : 60
+      storage_class = "GLACIER"
+    }
 
     noncurrent_version_expiration {
-      noncurrent_days = var.this_is_development ? 14 : 180
+      noncurrent_days = 180
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+
+  rule {
+    id = "expire_transition_tls"
+
+    filter {
+      prefix = "backups_with_expiration_enabled/tls-cert-backups/"
+    }
+
+    expiration {
+      days = 180
+    }
+
+    transition {
+      days          = var.this_is_development ? 20 : 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 180
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "GLACIER"
     }
     status = "Enabled"
   }
