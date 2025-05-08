18611832424a353de6342d8579328e4b736bc1e4
Venkat <venkata@venkatamutyala.com>
2025-05-08 08:25:46 -0700

    chore: delete migration-captain-data.tf

--- migration-captain-data.tf
+++
@@ -1,5 +0,0 @@
-# Remove this file entirely after applying/deploying!
-# Removes old items from terraform state without actually deleting them from S3
-removed {
-  from = aws_s3_object_copy.migrated_objects
-}
