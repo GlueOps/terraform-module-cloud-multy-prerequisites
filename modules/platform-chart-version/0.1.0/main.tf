025c69d95a90f710889a50582aa4993dbb5697a1
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-06-19 11:54:48 +0000

    fix: add codespace version to glueops.yaml

--- modules/platform-chart-version/0.1.0/main.tf
+++ modules/platform-chart-version/0.1.0/main.tf
@@ -10,14 +10,20 @@ variable "glueops_platform_version" {
   nullable    = false
 }
 
+variable "codespace_version" {
+  description = "Codespace version"
+  type        = string
+  nullable    = false
+}
 
 data "local_file" "version" {
   filename = "${path.module}/version.tpl"
 }
 
 output "platform_versions" {
-  value = replace(replace(
+  value = replace(replace(replace(
     data.local_file.version.content,
     "glueops_platform_helm_chart_version_placeholder", "${var.glueops_platform_version}"),
-  "argocd_helm_chart_version_placeholder", "${var.argocd_helm_chart_version}")
+    "argocd_helm_chart_version_placeholder", "${var.argocd_helm_chart_version}"),
+    "codespace_version_placeholder", "${var.codespace_version}")
 }
