d65c952c4e33d40ad7d01c1a787e0cb660b9f8ea
null <prasanth@runtimez.com>
2026-04-06 15:46:35 +0000

    Feat: updated the gatekeeper version

--- variables.tf
+++ variables.tf
@@ -159,7 +159,8 @@ locals {
   calico_ctl_version        = "v3.31.4"
   tigera_operator_version   = "v1.40.7"
   terraform_module_version  = "v0.48.0"
-  gatekeeper_tag            = "v0.1.0@sha256:969ac53b93a8ba2524494adbfd4587da5537c0c8d7232a5f4f5d67dcfe66874a"
+  gatekeeper_tag            = "v0.1.1@sha256:33f96e0ecc628078c00c68722a670fb72693860219219972503df0ee2c6a3ece"
+  gatekeeper_platform_allowed_namespaces = "glueops-core,${var.cluster_environments}"
 }
 
 variable "opsgenie_emails" {
